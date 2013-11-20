d16feaee112cdcb9ec0004900a88c1dd97017f3b
malcolm <mparrish91@gmail.com>
2013-11-20 01:05:07 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
+
+
 
   get "fuzzybunnies/", to: "static_pages#home"
+
+
   
   end
