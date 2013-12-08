da0813b281b6bb9f2565b9375d4afbfdf072c31c
malcolm <mparrish91@gmail.com>
2013-12-08 15:46:11 -0600

    add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
